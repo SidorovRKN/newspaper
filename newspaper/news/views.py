6976dfb682bbe7d7099d234f559761282dd82c28
kola <kolya.sidorov00@gmail.com>
2021-12-01 06:51:27 +0300

    0_4

--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -1,22 +1,45 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from .models import *
 
 menu = [
-        {'title': 'o saite', 'url_name': 'about'},
-        {'title': 'Dobavit statyu', 'url_name': 'add_page'},
-        {'title': 'Feedback', 'url_name': 'contact'},
-        {'title': 'Log In', 'url_name': 'login'},
-       ]
+    {'title': 'o saite', 'url_name': 'about'},
+    {'title': 'Dobavit statyu', 'url_name': 'add_page'},
+    {'title': 'Feedback', 'url_name': 'contact'},
+    {'title': 'Log In', 'url_name': 'login'},
+]
 
 
 def index(request):
     posts = News.objects.all()
-    context =  {'menu': menu, 'posts': posts, 'title': 'главная страница'}
+    cats = Category.objects.all()
+    context = {
+        'menu': menu,
+        'posts': posts,
+        'title': 'главная страница',
+        'cats': cats,
+        'cat_selected': 0,
+    }
     return render(request, 'news/index.html', context=context)
+
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def show_category(request, cat_id):
+    posts = News.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) == 0:
+        raise Http404
+    context = {
+        'menu': menu,
+        'posts': posts,
+        'title': 'рубрика',
+        'cats': cats,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'news/index.html', context=context)
 
 
 def about(request):
@@ -26,9 +49,10 @@ def about(request):
 def addpage(request):
     return HttpResponse("Добавление статьи")
 
+
 def contact(request):
     return HttpResponse("Обратная связь")
 
+
 def login(request):
     return HttpResponse("Авторизация")
-
